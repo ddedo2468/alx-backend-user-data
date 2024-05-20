dd4aa07904a9aa1189e239dea12123f3358b8f2f
Abdullah <abdallahmosbah25@gmail.com>
2024-05-21 01:38:35 +0300

    Task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,12 @@ def unauthorized() -> None:
       - unauthorized err
     """
     abort(401)
+
+
+@app_views.route("/forbidden/", strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - forbidden err
+    """
+    abort(403)
