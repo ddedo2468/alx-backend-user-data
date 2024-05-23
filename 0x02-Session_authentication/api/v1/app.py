7e8c144cc6473325cab4dd764c3e75cb77640a1f
Abdullah <abdallahmosbah25@gmail.com>
2024-05-23 21:48:33 +0300

    Task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,16 +13,23 @@ import os
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+
 auth_type = getenv("AUTH_TYPE", "auth")
 if auth_type == "auth":
     auth = Auth()
 if auth_type == "basic_auth":
     auth = BasicAuth()
+
+if auth_type == "session_auth":
+    auth = SessionAuth()
+
+
 
 
 @app.errorhandler(401)
